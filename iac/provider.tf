c8275a4b4f08b0f467e93a8ba6be0ed344b99c89
Marko Gusic <markoggusic@gmail.com>
2024-12-30 14:11:06 +0100

    small fix, change name of folder in s3 backend

--- iac/provider.tf
+++ iac/provider.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3"{
         bucket = "mg-terraform-state-storage"
-        key = "project-networking/terraform.tfstate"
+        key = "project1-networking/terraform.tfstate"
         region = "aws_region_placeholder"
         access_key = "access_key_placeholder"
         secret_key = "secret_key_placeholder"
