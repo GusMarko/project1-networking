590cad0965502bc916dd0a81be17a42a7968768f
Marko Gusic <markoggusic@gmail.com>
2025-01-07 10:16:47 +0100

    aa

--- iac/gateways.tf
+++ iac/gateways.tf
@@ -11,7 +11,7 @@ resource "aws_internet_gateway" "igw" {
 # NAT Gateway
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.eip.id
-  subnet_id     = aws_subnet.private.id
+  subnet_id     = aws_subnet.public.id
 
   tags = {
     Name = "NAT-gw"
